0583a1347b8be47498ce3c4de5a38261c685dfa9
Paulos Thomas <paulost72@gmail.com>
2021-06-15 19:09:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,17 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  ara
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
